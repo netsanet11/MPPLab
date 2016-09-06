db6fe472e00d19254c74793350a83220921a185a
netsanet11 <nmtame11@gmail.com>
2016-09-06 16:12:03 -0700

    Project Management System

---
+++ Project.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Project {
+	int productID;
+	String productName;
+	String version;
+
+
+public class Employee{
+
+String staffID;
+String FName;
+String LName;
+}
+
+public class Feature{
+int featureId;
+String description;
+int priority;
+String timeRemaining;
+String timeCompleted;
+}
+
+public class Sprint{
+int sprintId;
+String sprintName;
+String sprintDescription;
+
+
+}
+
+public class Release{
+
+int releaseId;
+String releaseName;
+Date relaseDate;
+String versionNumber;
+String releaseDescription;
+
+}
+
+public class Report{
+
+	String reportId;
+	String reportName;
+Sprint sprint;
+	String description;
+	
+
+}
+}
